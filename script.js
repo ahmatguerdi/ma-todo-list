b5228767ba999222730c877e891827a4f14e30fa
ahmatguerdi <ahmatguerdi9@gmail.com>
2025-06-17 12:05:28 +0100

    j'ai implémenté la fonction d'ajout de tâches lorsque l'utilisateur entre une valeur

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
- 
+let inputField = document.getElementById("inputField");
+let tasks = document.getElementById("tasks");
+// console.log(tasks, inputField);
+
+const addTask = () => {
+  let task = inputField.value.trim();
+  const li = document.createElement("li");  
+  li.innerHTML = `
+  <label>
+    <input type="checkbox" />
+    <span>${task}</span>
+  </label>
+  <span class="editBtn" title="Modifier"><i class="fa-solid fa-pen-to-square"></i></span>
+  <span class="deleteBtn" title="Supprimer"><i class="fa-solid fa-trash"></i></span>
+    `
+  tasks.appendChild(li);
+  inputField.value = "";
+
+  const checkbox = li.querySelector("input");
+//   console.log(checkbox);
+checkbox.addEventListener("click", () =>{
+   let span1= li.querySelectorAll("span")[0];
+   span1.classList.toggle("checked");
+//    console.log(span1);
+   
+})
+  
+};
